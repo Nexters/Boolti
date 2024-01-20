8f6b0718782a60e7b22346a9cc04496be69a20cc
mangbaam <pmb0836@gmail.com>
2024-01-21 00:50:25 +0900

    Boolti-8 chore: dependency cycle 해결을 위한 Authenticator 의존 없는 OkHttpClient, Retrofit 작성

--- presentation/src/main/java/com/nexters/boolti/presentation/screen/MainViewModel.kt
+++ presentation/src/main/java/com/nexters/boolti/presentation/screen/MainViewModel.kt
@@ -4,16 +4,17 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.nexters.boolti.domain.repository.AuthRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
-//    authRepository: AuthRepository,
+    authRepository: AuthRepository,
 ) : ViewModel() {
-    val loggedIn = MutableStateFlow<Boolean?>(false)
+    val loggedIn = authRepository.loggedIn.stateIn(
+        viewModelScope,
+        SharingStarted.WhileSubscribed(5000),
+        null,
+    )
 }

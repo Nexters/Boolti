cf33b2165eccc9355e6b97764d2f27614edc2dc8
algosketch <algosketch@gmail.com>
2024-01-24 16:44:13 +0900

    chore : font 추가

--- presentation/src/main/java/com/nexters/boolti/presentation/theme/Type.kt
+++ presentation/src/main/java/com/nexters/boolti/presentation/theme/Type.kt
@@ -2,9 +2,17 @@ package com.nexters.boolti.presentation.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.googlefonts.GoogleFont
 import androidx.compose.ui.unit.sp
+import com.nexters.boolti.presentation.R
+
+val pretendardFamily = FontFamily(
+    Font(R.font.pretendard_regular, FontWeight.Normal),
+    Font(R.font.pretendard_semi_bold, FontWeight.SemiBold)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(

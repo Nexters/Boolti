98559943c54968fb4d2daf0763cf35f28e8b1aa2
algosketch <algosketch@gmail.com>
2024-01-24 17:07:33 +0900

    refactor : 디자인 시스템을 이해하기 쉽도록 리팩토링

--- presentation/src/main/java/com/nexters/boolti/presentation/theme/Type.kt
+++ presentation/src/main/java/com/nexters/boolti/presentation/theme/Type.kt
@@ -8,73 +8,91 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import com.nexters.boolti.presentation.R
 
+/**
+ * Figma 디자인의 Design system을 참고하세요.
+ */
+
 val pretendardFamily = FontFamily(
     Font(R.font.pretendard_regular, FontWeight.Normal),
     Font(R.font.pretendard_semi_bold, FontWeight.SemiBold)
 )
 
+private val headline3 = TextStyle(
+    fontFamily = pretendardFamily,
+    fontWeight = FontWeight.SemiBold,
+    fontSize = 28.sp,
+    lineHeight = 40.sp,
+)
+private val headline2 = TextStyle(
+    fontFamily = pretendardFamily,
+    fontWeight = FontWeight.SemiBold,
+    fontSize = 24.sp,
+    lineHeight = 32.sp,
+)
+private val headline1 = TextStyle(
+    fontFamily = pretendardFamily,
+    fontWeight = FontWeight.SemiBold,
+    fontSize = 20.sp,
+    lineHeight = 28.sp,
+)
+
+private val subhead2 = TextStyle(
+    fontFamily = pretendardFamily,
+    fontWeight = FontWeight.SemiBold,
+    fontSize = 18.sp,
+    lineHeight = 26.sp,
+)
+private val subhead1 = TextStyle(
+    fontFamily = pretendardFamily,
+    fontWeight = FontWeight.SemiBold,
+    fontSize = 16.sp,
+    lineHeight = 22.sp,
+)
+
+private val body4 = TextStyle(
+    fontFamily = pretendardFamily,
+    fontWeight = FontWeight.Normal,
+    fontSize = 18.sp,
+    lineHeight = 26.sp,
+)
+private val body3 = TextStyle(
+    fontFamily = pretendardFamily,
+    fontWeight = FontWeight.Normal,
+    fontSize = 16.sp,
+    lineHeight = 22.sp,
+)
+private val body2 = TextStyle(
+    fontFamily = pretendardFamily,
+    fontWeight = FontWeight.Normal,
+    fontSize = 15.sp,
+    lineHeight = 21.sp,
+)
+private val body1 = TextStyle(
+    fontFamily = pretendardFamily,
+    fontWeight = FontWeight.Normal,
+    fontSize = 14.sp,
+    lineHeight = 20.sp,
+)
+
+private val caption = TextStyle(
+    fontFamily = pretendardFamily,
+    fontWeight = FontWeight.Normal,
+    fontSize = 12.sp,
+    lineHeight = 18.sp,
+)
+
 val Typography = Typography(
-    headlineLarge = TextStyle(
-        fontFamily = pretendardFamily,
-        fontWeight = FontWeight.SemiBold,
-        fontSize = 28.sp,
-        lineHeight = 40.sp,
-    ),
-    headlineMedium = TextStyle(
-        fontFamily = pretendardFamily,
-        fontWeight = FontWeight.SemiBold,
-        fontSize = 24.sp,
-        lineHeight = 32.sp,
-    ),
-    headlineSmall = TextStyle(
-        fontFamily = pretendardFamily,
-        fontWeight = FontWeight.SemiBold,
-        fontSize = 20.sp,
-        lineHeight = 28.sp,
-    ),
+    headlineLarge = headline3,
+    headlineMedium = headline2,
+    headlineSmall = headline1,
 
-    titleLarge = TextStyle(
-        fontFamily = pretendardFamily,
-        fontWeight = FontWeight.SemiBold,
-        fontSize = 18.sp,
-        lineHeight = 26.sp,
-    ),
-    titleMedium = TextStyle(
-        fontFamily = pretendardFamily,
-        fontWeight = FontWeight.SemiBold,
-        fontSize = 16.sp,
-        lineHeight = 22.sp,
-    ),
-    titleSmall = TextStyle(
-        fontFamily = pretendardFamily,
-        fontWeight = FontWeight.Normal,
-        fontSize = 18.sp,
-        lineHeight = 26.sp,
-    ),
+    titleLarge = subhead2,
+    titleMedium = subhead1,
 
-    bodyLarge = TextStyle(
-        fontFamily = pretendardFamily,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 22.sp,
-    ),
-    bodyMedium = TextStyle(
-        fontFamily = pretendardFamily,
-        fontWeight = FontWeight.Normal,
-        fontSize = 15.sp,
-        lineHeight = 21.sp,
-    ),
-    bodySmall = TextStyle(
-        fontFamily = pretendardFamily,
-        fontWeight = FontWeight.Normal,
-        fontSize = 14.sp,
-        lineHeight = 20.sp,
-    ),
+    titleSmall = body4,
+    bodyLarge = body3,
+    bodyMedium = body2,
+    bodySmall = body1,
 
-    labelMedium = TextStyle(
-        fontFamily = pretendardFamily,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp,
-        lineHeight = 18.sp,
-    )
+    labelMedium = caption,
 )

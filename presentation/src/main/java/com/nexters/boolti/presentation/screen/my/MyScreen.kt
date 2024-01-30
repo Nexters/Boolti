a7b2b8da70e9d8c1965100077d67e857c0b67a8f
algosketch <algosketch@gmail.com>
2024-01-31 04:31:43 +0900

    feat : 테스트용 버튼 추가

--- presentation/src/main/java/com/nexters/boolti/presentation/screen/my/MyScreen.kt
+++ presentation/src/main/java/com/nexters/boolti/presentation/screen/my/MyScreen.kt
@@ -1,24 +1,37 @@
 package com.nexters.boolti.presentation.screen.my
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 
 @Composable
 fun MyScreen(
     modifier: Modifier = Modifier,
-    viewModel: MyViewModel = hiltViewModel()) {
+    viewModel: MyViewModel = hiltViewModel(),
+    requireLogin: () -> Unit,
+) {
     Box(
         modifier = modifier,
         contentAlignment = Alignment.Center,
     ) {
-        Button(onClick = { viewModel.logout() }) {
-            Text(text = "로그아웃", style = MaterialTheme.typography.headlineMedium)
+        Column {
+            Button(onClick = requireLogin) {
+                Text(text = "로그인 화면으로", style = MaterialTheme.typography.headlineMedium)
+            }
+            Button(
+                modifier = Modifier.padding(top = 20.dp),
+                onClick = viewModel::logout
+            ) {
+                Text(text = "로그아웃", style = MaterialTheme.typography.headlineMedium)
+            }
         }
     }
 }

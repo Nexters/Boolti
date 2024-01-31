dc46fb609e006f7c2cb0fcf11e0367dd2a143887
algosketch <algosketch@gmail.com>
2024-02-01 04:23:37 +0900

    fix : 예매하기 버튼 MainButton으로 변경 및 간격 조정

--- presentation/src/main/java/com/nexters/boolti/presentation/screen/show/ShowDetailScreen.kt
+++ presentation/src/main/java/com/nexters/boolti/presentation/screen/show/ShowDetailScreen.kt
@@ -1,17 +1,14 @@
 package com.nexters.boolti.presentation.screen.show
 
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.BottomSheetDefaults
 import androidx.compose.material3.BottomSheetScaffold
-import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.material3.rememberBottomSheetScaffoldState
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
@@ -20,10 +17,14 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.nexters.boolti.presentation.R
+import com.nexters.boolti.presentation.component.MainButton
 import com.nexters.boolti.presentation.screen.ticketing.ChooseTicketBottomSheetContent
 import com.nexters.boolti.presentation.theme.Grey70
+import com.nexters.boolti.presentation.theme.marginHorizontal
 import kotlinx.coroutines.launch
 import timber.log.Timber
 
@@ -62,27 +63,25 @@ fun ShowDetailScreen(
                 color = Grey70
             )
         },
+        sheetPeekHeight = 0.dp,
         sheetShape = RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp)
     ) { innerPadding ->
         Box(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(innerPadding)
+                .padding(innerPadding),
+            contentAlignment = Alignment.BottomCenter,
         ) {
-            Button(
+            MainButton(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .align(Alignment.BottomCenter)
-                    .padding(start = 20.dp, end = 20.dp, bottom = 24.dp),
-                shape = RoundedCornerShape(4.dp),
-                contentPadding = PaddingValues(12.dp),
-                onClick = {
-                    scope.launch {
-                        scaffoldState.bottomSheetState.expand()
-                    }
-                }
+                    .padding(horizontal = marginHorizontal, vertical = 8.dp)
+                    .padding(bottom = 34.dp),
+                label = stringResource(id = R.string.ticketing_button_label)
             ) {
-                Text(text = "예매하기", style = MaterialTheme.typography.bodyLarge)
+                scope.launch {
+                    scaffoldState.bottomSheetState.expand()
+                }
             }
         }
     }

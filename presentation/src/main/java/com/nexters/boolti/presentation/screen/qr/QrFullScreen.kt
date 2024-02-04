294765572c7309055bc044374de843104bc26a82
mangbaam <pmb0836@gmail.com>
2024-02-04 18:32:31 +0900

    Boolti-23 feat: QR 위에 로고 표시

--- presentation/src/main/java/com/nexters/boolti/presentation/screen/qr/QrFullScreen.kt
+++ presentation/src/main/java/com/nexters/boolti/presentation/screen/qr/QrFullScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -15,10 +16,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
 import com.nexters.boolti.presentation.R
 import com.nexters.boolti.presentation.screen.ticket.TicketState
 import com.nexters.boolti.presentation.theme.Grey90
@@ -30,16 +33,20 @@ fun QrFullScreen(
     onClose: () -> Unit,
 ) {
     val data = "BooltiTicket1"
-    Box(
+    ConstraintLayout(
         modifier = modifier
             .background(Color.White)
             .fillMaxSize()
     ) {
+        val (closeButton, logo, qr) = createRefs()
+
         IconButton(
             onClick = onClose,
             modifier = Modifier
-                .align(Alignment.TopEnd)
-                .padding(vertical = 10.dp, horizontal = 20.dp),
+                .constrainAs(closeButton) {
+                    top.linkTo(parent.top, margin = 10.dp)
+                    end.linkTo(parent.end, margin = 20.dp)
+                }
         ) {
             Icon(
                 painter = painterResource(R.drawable.ic_close),
@@ -50,7 +57,22 @@ fun QrFullScreen(
 
         Image(
             modifier = Modifier
-                .align(Alignment.Center)
+                .constrainAs(logo) {
+                    centerHorizontallyTo(parent)
+                    bottom.linkTo(qr.top, margin = 8.dp)
+                }
+                .width(84.dp),
+            painter = painterResource(R.drawable.boolti_logo),
+            colorFilter = ColorFilter.tint(Color.Black),
+            contentScale = ContentScale.FillWidth,
+            contentDescription = null,
+        )
+        Image(
+            modifier = Modifier
+                .constrainAs(qr) {
+                    centerVerticallyTo(parent)
+                    centerHorizontallyTo(parent)
+                }
                 .background(Color.White)
                 .padding(8.dp),
             painter = rememberQrBitmapPainter(

d06b9c2e5585eee786dd7d94c57cd6ea5830758e
algosketch <algosketch@gmail.com>
2024-02-08 18:15:17 +0900

    feat : UI에 필요한 요소 삽입

--- presentation/src/main/java/com/nexters/boolti/presentation/screen/my/MyScreen.kt
+++ presentation/src/main/java/com/nexters/boolti/presentation/screen/my/MyScreen.kt
@@ -1,16 +1,15 @@
 package com.nexters.boolti.presentation.screen.my
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import coil.compose.AsyncImage
 
 @Composable
 fun MyScreen(
@@ -18,20 +17,29 @@ fun MyScreen(
     viewModel: MyViewModel = hiltViewModel(),
     requireLogin: () -> Unit,
 ) {
-    Box(
+    Column(
         modifier = modifier,
-        contentAlignment = Alignment.Center,
     ) {
-        Column {
-            Button(onClick = requireLogin) {
-                Text(text = "로그인 화면으로", style = MaterialTheme.typography.headlineMedium)
-            }
-            Button(
-                modifier = Modifier.padding(top = 20.dp),
-                onClick = viewModel::logout
-            ) {
-                Text(text = "로그아웃", style = MaterialTheme.typography.headlineMedium)
+        Row {
+//            AsyncImage(model = "", contentDescription = null)
+            Column {
+                Text(text = "김불티")
+                Text(text = "boolti1234@gmail.com")
             }
         }
+
+        MyButton(text = "예매 내역", onClick = {})
+        MyButton(text = "QR 스캔", onClick = {})
+
+        Text(
+            modifier = Modifier.clickable(onClick = viewModel::logout),
+            text = "로그아웃", style = MaterialTheme.typography.headlineMedium
+        )
     }
+}
+
+// todo : 적절한 이름이 없을까...?
+@Composable
+private fun MyButton(text: String, onClick: () -> Unit) {
+
 }

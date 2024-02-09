3e1f5e5cde8645f2e6dc3f7df51c14ca6abea3a9
algosketch <algosketch@gmail.com>
2024-02-10 01:31:19 +0900

    refactor : copy button 제사용을 위한 추출

---
+++ presentation/src/main/java/com/nexters/boolti/presentation/component/CopyButton.kt
@@ -0,0 +1,43 @@
+package com.nexters.boolti.presentation.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.nexters.boolti.presentation.R
+import com.nexters.boolti.presentation.theme.Grey85
+
+@Composable
+fun CopyButton(
+    label: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Row(
+        modifier = modifier
+            .clip(shape = RoundedCornerShape(4.dp))
+            .background(color = Grey85)
+            .padding(horizontal = 12.dp, vertical = 6.dp)
+            .clickable(onClick = onClick)
+    ) {
+        Icon(
+            painter = painterResource(id = R.drawable.ic_copy),
+            contentDescription = label
+        )
+        Text(
+            modifier = Modifier.padding(start = 6.dp),
+            text = label,
+            style = MaterialTheme.typography.labelMedium
+        )
+    }
+}

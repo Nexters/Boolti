60afb6657f7a8762a18527b73875984de2492662
algosketch <algosketch@gmail.com>
2024-02-09 20:16:19 +0900

    feat : 티켓 예매 기간 border 그라데이션

--- presentation/src/main/java/com/nexters/boolti/presentation/screen/show/ShowDetailScreen.kt
+++ presentation/src/main/java/com/nexters/boolti/presentation/screen/show/ShowDetailScreen.kt
@@ -28,6 +28,7 @@ import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -36,9 +37,11 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.platform.LocalClipboardManager
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
@@ -46,12 +49,14 @@ import androidx.compose.ui.res.stringArrayResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.nexters.boolti.domain.model.ShowDetail
 import com.nexters.boolti.domain.model.ShowState
 import com.nexters.boolti.presentation.R
+import com.nexters.boolti.presentation.component.DottedDivider
 import com.nexters.boolti.presentation.component.MainButton
 import com.nexters.boolti.presentation.component.ToastSnackbarHost
 import com.nexters.boolti.presentation.screen.ticketing.ChooseTicketBottomSheet
@@ -404,21 +409,79 @@ private fun TicketReservationPeriod(
     val startDayIndex = startDate.dayOfWeek.value
     val endDayIndex = endDate.dayOfWeek.value
 
+    var size by remember {
+        mutableStateOf(IntSize.Zero)
+    }
+    val gradientOffset by remember {
+        derivedStateOf {
+            Offset(
+                size.width.toFloat() / 32,
+                size.height.toFloat()
+            )
+        }
+    }
     Column(
         modifier = modifier
             .fillMaxWidth()
-            .clip(shape = RoundedCornerShape(8.dp))
-            .border(shape = RoundedCornerShape(8.dp), color = Color.White, width = 1.dp)
+            .clip(shape = ReservationPeriodShape(radius = 8.dp, circleRadius = 10.dp))
+            .border(
+                shape = ReservationPeriodShape(radius = 8.dp, circleRadius = 10.dp),
+                brush = Brush.linearGradient(
+                    colorStops = arrayOf(
+                        Pair(0.25f, Color(0xFFDBCBCB)),
+                        Pair(0.75f, Color.Transparent),
+                    ),
+                    end = gradientOffset,
+                ),
+                width = 1.dp,
+            )
+            .border(
+                shape = ReservationPeriodShape(radius = 8.dp, circleRadius = 10.dp),
+                brush = Brush.linearGradient(
+                    colorStops = arrayOf(
+                        Pair(0.25f, Color(0xFFC2C2C2).copy(alpha = 0.6538f)),
+                        Pair(0.75f, Color.Transparent),
+                    ),
+                    start = Offset(size.width.toFloat(), size.height.toFloat()),
+                    end = Offset(size.width.toFloat(), size.height.toFloat()) - gradientOffset
+                ),
+                width = 1.dp,
+            )
             .background(color = Grey70)
-            .padding(vertical = 12.dp),
+            .padding(vertical = 12.dp)
+            .onSizeChanged { size = it },
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        Text(
-            stringResource(id = R.string.ticketing_period),
-            style = MaterialTheme.typography.titleMedium.copy(color = Grey15)
-        )
-        Divider(
-            modifier = Modifier.padding(vertical = 10.dp), thickness = 1.dp, color = Color.Black
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Box(
+                modifier = Modifier
+                    .size(5.dp)
+                    .clip(shape = RoundedCornerShape(10.dp))
+                    .background(color = Grey30)
+            )
+            Text(
+                modifier = Modifier.padding(horizontal = 8.5.dp),
+                text = stringResource(id = R.string.ticketing_period),
+                style = MaterialTheme.typography.titleMedium.copy(color = Grey15)
+            )
+            Box(
+                modifier = Modifier
+                    .size(5.dp)
+                    .clip(shape = RoundedCornerShape(10.dp))
+                    .background(color = Grey30)
+            )
+        }
+        DottedDivider(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 10.dp),
+            color = Color.Black,
+            thickness = 1.dp,
+            dash = 6.dp,
+            spacedBy = 6.dp,
         )
         Text(
             // ex. 2023.12.01 (토) - 2024.01.20 (월)

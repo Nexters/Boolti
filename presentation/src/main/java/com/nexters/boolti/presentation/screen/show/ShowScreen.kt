8e8a4deae3decdd486e751b1ba5558f7a67708c5
algosketch <algosketch@gmail.com>
2024-02-11 06:49:42 +0900

    feat : 모션 레이아웃 적용 (근데 이제 고장을 곁들인)

--- presentation/src/main/java/com/nexters/boolti/presentation/screen/show/ShowScreen.kt
+++ presentation/src/main/java/com/nexters/boolti/presentation/screen/show/ShowScreen.kt
@@ -3,15 +3,13 @@ package com.nexters.boolti.presentation.screen.show
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.rememberLazyGridState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
@@ -23,22 +21,35 @@ import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
+import androidx.compose.ui.input.nestedscroll.NestedScrollSource
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.layout.layoutId
+import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ExperimentalMotionApi
+import androidx.constraintlayout.compose.MotionLayout
+import androidx.constraintlayout.compose.MotionScene
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.nexters.boolti.presentation.R
 import com.nexters.boolti.presentation.component.ShowFeed
 import com.nexters.boolti.presentation.theme.Grey15
-import com.nexters.boolti.presentation.theme.Grey50
 import com.nexters.boolti.presentation.theme.Grey60
 import com.nexters.boolti.presentation.theme.Grey70
 import com.nexters.boolti.presentation.theme.Grey85
@@ -52,27 +63,51 @@ fun ShowScreen(
     viewModel: ShowViewModel = hiltViewModel()
 ) {
     val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+    var offsetY by remember { mutableFloatStateOf(0f) }
+    val appbarHeight = 116 * 2.625f//by remember { mutableFloatStateOf(0f) }
+    var progress by remember { mutableFloatStateOf(0f) }
+    val lazyGridState = rememberLazyGridState()
+    val nestedScrollConnection = remember {
+        object : NestedScrollConnection {
+            override fun onPreScroll(available: Offset, source: NestedScrollSource): Offset {
+
+                // LazyGrid 스크롤을 내려야 할 때
+                if (progress == 1.0f && available.y < 0) {
+                    return Offset.Zero
+                }
+
+                // LazyGrid 스크롤을 올려야 할 때
+                if (progress == 1.0f && available.y > 0 && lazyGridState.canScrollBackward) {
+                    return Offset.Zero
+                }
+
+                // 앱바를 움직여야 할 때!
+                offsetY += available.y
+                offsetY = offsetY.coerceIn(-appbarHeight, 0f)
+                progress = -offsetY / appbarHeight
+                println("프로그레스 : $progress")
+
+                return available
+            }
+        }
+    }
 
     Scaffold(
-        modifier = modifier,
-        topBar = {
-            ShowAppBar(
-                text = uiState.keyword,
-                onKeywordChanged = viewModel::updateKeyword,
-                search = viewModel::search,
-            )
-        },
+        modifier = modifier.nestedScroll(nestedScrollConnection),
     ) { innerPadding ->
         Box(
             modifier = Modifier.padding(innerPadding),
-            contentAlignment = Alignment.Center,
+            contentAlignment = Alignment.TopCenter,
         ) {
             LazyVerticalGrid(
-                modifier = Modifier.padding(horizontal = marginHorizontal),
+                modifier = Modifier
+                    .padding(horizontal = marginHorizontal)
+                    .padding(top = 208.dp - ((appbarHeight / 2.625f) * progress).dp),
                 columns = GridCells.Adaptive(minSize = 150.dp),
                 horizontalArrangement = Arrangement.spacedBy(15.dp),
                 verticalArrangement = Arrangement.spacedBy(28.dp),
                 contentPadding = PaddingValues(top = 12.dp),
+                state = lazyGridState,
             ) {
                 items(
                     count = uiState.shows.size,
@@ -84,25 +119,48 @@ fun ShowScreen(
                     )
                 }
             }
+            ShowAppBar(
+                progress = progress,
+                text = uiState.keyword,
+                onKeywordChanged = viewModel::updateKeyword,
+                onSizeChanged = { size ->
+//                    appbarHeight = size.height.toFloat()
+                },
+                search = viewModel::search,
+            )
         }
     }
 }
 
+@OptIn(ExperimentalMotionApi::class)
 @Composable
 fun ShowAppBar(
+    progress: Float,
     text: String,
     onKeywordChanged: (keyword: String) -> Unit,
+    onSizeChanged: (size: IntSize) -> Unit,
     search: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    Column(
+    val context = LocalContext.current
+    val motionScene = remember {
+        context.resources
+            .openRawResource(R.raw.home_motion_scene)
+            .readBytes()
+            .decodeToString()
+    }
+
+    MotionLayout(
         modifier = modifier
             .fillMaxWidth()
-            .padding(horizontal = marginHorizontal),
+            .padding(horizontal = marginHorizontal)
+            .onSizeChanged(onSizeChanged = onSizeChanged),
+        motionScene = MotionScene(content = motionScene),
+        progress = progress,
     ) {
         Text(
             modifier = Modifier
-                .padding(top = 40.dp)
+                .layoutId("sub_title")
                 .fillMaxWidth(),
             text = stringResource(id = R.string.home_sub_title, "닉네임"), // todo : 실 유저 네임으로 변경
             style = TextStyle(
@@ -113,12 +171,12 @@ fun ShowAppBar(
             ),
         )
         SearchBar(
+            modifier = Modifier
+                .layoutId("search_bar")
+                .padding(vertical = 16.dp),
             text = text,
             onKeywordChanged = onKeywordChanged,
             search = search,
-            modifier = Modifier
-                .padding(vertical = 16.dp)
-                .padding(top = 12.dp)
         )
     }
 }

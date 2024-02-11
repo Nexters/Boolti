2a69d725f621d08871335af82f917d96708d5b5d
algosketch <algosketch@gmail.com>
2024-02-11 23:15:59 +0900

    feat : viewModel까지 reservation 가져오기

---
+++ presentation/src/main/java/com/nexters/boolti/presentation/screen/reservations/ReservationDetailUiState.kt
@@ -0,0 +1,15 @@
+package com.nexters.boolti.presentation.screen.reservations
+
+import com.nexters.boolti.domain.model.ReservationDetail
+
+sealed interface ReservationDetailUiState {
+    data object Loading : ReservationDetailUiState
+
+    data class Success(
+        val reservation: ReservationDetail,
+    ) : ReservationDetailUiState
+
+    data class Error(
+        val message: String = ""
+    ) : ReservationDetailUiState
+}
---
+++ presentation/src/main/java/com/nexters/boolti/presentation/screen/reservations/ReservationViewModel.kt
@@ -0,0 +1,48 @@
+package com.nexters.boolti.presentation.screen.reservations
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.nexters.boolti.domain.repository.ReservationRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.update
+import javax.inject.Inject
+
+@HiltViewModel
+class ReservationViewModel @Inject constructor(
+    savedStateHandle: SavedStateHandle,
+    private val reservationRepository: ReservationRepository,
+) : ViewModel() {
+    private val reservationId: String = checkNotNull(savedStateHandle["reservationId"]) {
+        "reservationId가 전달되어야 합니다."
+    }
+    private val _uiState: MutableStateFlow<ReservationDetailUiState> =
+        MutableStateFlow(ReservationDetailUiState.Loading)
+    val uiState: StateFlow<ReservationDetailUiState> = _uiState.asStateFlow()
+
+    init {
+        fetchReservation()
+    }
+
+    private fun fetchReservation() {
+        reservationRepository.findReservationById(reservationId)
+            .onStart {
+                _uiState.update { ReservationDetailUiState.Loading }
+            }
+            .onEach { reservation ->
+                _uiState.update { ReservationDetailUiState.Success(reservation) }
+            }
+            .catch {
+                it.printStackTrace()
+                _uiState.update { ReservationDetailUiState.Error() }
+            }
+            .launchIn(viewModelScope)
+    }
+}

39dd7fd8ed572a6d5589cff060d4943845054456
algosketch <algosketch@gmail.com>
2024-02-12 21:25:18 +0900

    feat : Pager로 환불 화면 연결

--- presentation/src/main/java/com/nexters/boolti/presentation/screen/refund/RefundScreen.kt
+++ presentation/src/main/java/com/nexters/boolti/presentation/screen/refund/RefundScreen.kt
@@ -1,10 +1,14 @@
 package com.nexters.boolti.presentation.screen.refund
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -12,6 +16,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -25,12 +30,17 @@ import com.nexters.boolti.presentation.theme.Grey70
 import com.nexters.boolti.presentation.theme.Grey85
 import com.nexters.boolti.presentation.theme.marginHorizontal
 import com.nexters.boolti.presentation.theme.point4
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun RefundScreen(
     onBackPressed: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
+    val scope = rememberCoroutineScope()
+    val pagerState = rememberPagerState { 2 }
+
     Scaffold(
         topBar = {
             BtAppBar(
@@ -40,50 +50,80 @@ fun RefundScreen(
         },
         modifier = modifier,
     ) { innerPadding ->
-        Column(
-            modifier = Modifier.padding(innerPadding)
-        ) {
-            Text(
-                modifier = Modifier
-                    .padding(top = 20.dp)
-                    .padding(horizontal = marginHorizontal),
-                text = stringResource(id = R.string.refund_reason_label),
-                style = point4,
-            )
-            TextField(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = marginHorizontal)
-                    .height(160.dp)
-                    .padding(top = 20.dp)
-                    .clip(shape = RoundedCornerShape(4.dp)),
-                value = "",
-                onValueChange = {},
-                textStyle = MaterialTheme.typography.bodyLarge.copy(color = Grey10),
-                colors = TextFieldDefaults.colors(
-                    focusedContainerColor = Grey85,
-                    unfocusedContainerColor = Grey85,
-                    focusedIndicatorColor = Color.Transparent,
-                    unfocusedIndicatorColor = Color.Transparent,
-                ),
-                shape = RoundedCornerShape(4.dp),
-                placeholder = {
-                    Text(
-                        text = stringResource(id = R.string.refund_reason_hint),
-                        style = MaterialTheme.typography.bodyLarge.copy(color = Grey70),
-                    )
-                }
-            )
-            Spacer(modifier = Modifier.weight(1.0f))
-            MainButton(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = marginHorizontal)
-                    .padding(bottom = 8.dp),
-                onClick = {},
-                enabled = false, // TODO 입력 여부
-                label = stringResource(id = R.string.next)
-            )
+        HorizontalPager(
+            modifier = Modifier.fillMaxSize(),
+            state = pagerState,
+            userScrollEnabled = false,
+        ) { index ->
+            if (index == 0) {
+                ReasonPage(
+                    onNextClick = {
+                        scope.launch {
+                            pagerState.animateScrollToPage(1)
+                        }
+                    },
+                    modifier = Modifier.padding(innerPadding)
+                )
+            } else {
+                RefundInfoPage()
+            }
         }
     }
+}
+
+@Composable
+fun ReasonPage(
+    onNextClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier
+    ) {
+        Text(
+            modifier = Modifier
+                .padding(top = 20.dp)
+                .padding(horizontal = marginHorizontal),
+            text = stringResource(id = R.string.refund_reason_label),
+            style = point4,
+        )
+        TextField(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = marginHorizontal)
+                .height(160.dp)
+                .padding(top = 20.dp)
+                .clip(shape = RoundedCornerShape(4.dp)),
+            value = "",
+            onValueChange = {},
+            textStyle = MaterialTheme.typography.bodyLarge.copy(color = Grey10),
+            colors = TextFieldDefaults.colors(
+                focusedContainerColor = Grey85,
+                unfocusedContainerColor = Grey85,
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+            ),
+            shape = RoundedCornerShape(4.dp),
+            placeholder = {
+                Text(
+                    text = stringResource(id = R.string.refund_reason_hint),
+                    style = MaterialTheme.typography.bodyLarge.copy(color = Grey70),
+                )
+            }
+        )
+        Spacer(modifier = Modifier.weight(1.0f))
+        MainButton(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = marginHorizontal)
+                .padding(bottom = 8.dp),
+            onClick = onNextClick,
+            enabled = true, // TODO 입력 여부
+            label = stringResource(id = R.string.next)
+        )
+    }
+}
+
+@Composable
+fun RefundInfoPage() {
+
 }

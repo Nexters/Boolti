62a8c755abc1654a6dbf2b0990be78f8444a6320
algosketch <algosketch@gmail.com>
2024-02-17 03:48:50 +0900

    fix : 복사 버튼 높이 변경

--- presentation/src/main/java/com/nexters/boolti/presentation/component/CopyButton.kt
+++ presentation/src/main/java/com/nexters/boolti/presentation/component/CopyButton.kt
@@ -3,12 +3,14 @@ package com.nexters.boolti.presentation.component
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
@@ -26,9 +28,11 @@ fun CopyButton(
     Row(
         modifier = modifier
             .clip(shape = RoundedCornerShape(4.dp))
+            .height(30.dp)
             .background(color = Grey85)
-            .padding(horizontal = 12.dp, vertical = 6.dp)
-            .clickable(onClick = onClick)
+            .padding(horizontal = 12.dp)
+            .clickable(onClick = onClick),
+        verticalAlignment = Alignment.CenterVertically,
     ) {
         Icon(
             painter = painterResource(id = R.drawable.ic_copy),

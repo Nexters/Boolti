aebcf101b1b22da3dfbb45502d31e3b9ae0d2641
algosketch <algosketch@gmail.com>
2024-03-26 21:16:03 +0900

    feat : 포그라운드 딥링크 처리

--- presentation/src/main/java/com/nexters/boolti/presentation/service/BtFirebaseMessagingService.kt
+++ presentation/src/main/java/com/nexters/boolti/presentation/service/BtFirebaseMessagingService.kt
@@ -8,12 +8,14 @@ import com.google.firebase.messaging.RemoteMessage
 import com.nexters.boolti.domain.repository.AuthRepository
 import com.nexters.boolti.presentation.R
 import com.nexters.boolti.presentation.extension.checkGrantedPermission
+import com.nexters.boolti.presentation.screen.DeepLinkEvent
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.launch
+import timber.log.Timber
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -23,6 +25,9 @@ class BtFirebaseMessagingService : FirebaseMessagingService() {
     @Inject
     lateinit var authRepository: AuthRepository
 
+    @Inject
+    lateinit var deepLinkEvent: DeepLinkEvent
+
     override fun onNewToken(token: String) {
         scope.launch {
             authRepository.sendFcmToken()
@@ -31,6 +36,19 @@ class BtFirebaseMessagingService : FirebaseMessagingService() {
 
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
         if(!checkGrantedPermission(Manifest.permission.POST_NOTIFICATIONS)) return
+
+        if (remoteMessage.data.isNotEmpty()) {
+            Timber.d("Message data payload: ${remoteMessage.data}")
+
+            // TODO : 서버에서 푸시 알림 타입을 확정하면 변경하기
+            val deepLink = when (remoteMessage.data["합의된 Key"]) {
+                else -> "https://app.boolti.in/home/tickets"
+            }
+
+            scope.launch {
+                deepLinkEvent.sendEvent(deepLink)
+            }
+        }
 
         remoteMessage.notification?.let { notification ->
             val defaultChannelId = getString(R.string.default_notification_channel_id)

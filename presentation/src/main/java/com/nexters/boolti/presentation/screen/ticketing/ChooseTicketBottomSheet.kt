9d204d84288a4bd8e7be670df0a5dd7fd844cb45
mangbaam <pmb0836@gmail.com>
2024-05-02 02:10:57 +0900

    style: 문구 관련 QA 수정

--- presentation/src/main/java/com/nexters/boolti/presentation/screen/ticketing/ChooseTicketBottomSheet.kt
+++ presentation/src/main/java/com/nexters/boolti/presentation/screen/ticketing/ChooseTicketBottomSheet.kt
@@ -222,7 +222,7 @@ private fun ChooseTicketBottomSheetContent2(
             )
             Spacer(modifier = Modifier.weight(1F))
             Text(
-                text = stringResource(R.string.format_total_price, ticket.ticket.price * ticketCount),
+                text = stringResource(R.string.format_price, ticket.ticket.price * ticketCount),
                 style = MaterialTheme.typography.titleLarge.copy(color = MaterialTheme.colorScheme.primary),
             )
         }

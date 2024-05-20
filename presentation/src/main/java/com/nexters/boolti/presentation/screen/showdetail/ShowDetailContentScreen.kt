b19a4cf77e5975aa5b305a58b42f3ac69e5def86
algosketch <algosketch@gmail.com>
2024-05-20 22:55:10 +0900

    feat : [공연 상세] 전체 내용 화면에서 url 하이라이팅 처리

--- presentation/src/main/java/com/nexters/boolti/presentation/screen/showdetail/ShowDetailContentScreen.kt
+++ presentation/src/main/java/com/nexters/boolti/presentation/screen/showdetail/ShowDetailContentScreen.kt
@@ -2,14 +2,19 @@ package com.nexters.boolti.presentation.screen.showdetail
 
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -24,8 +29,33 @@ fun ShowDetailContentScreen(
     modifier: Modifier = Modifier,
     viewModel: ShowDetailViewModel = hiltViewModel(),
 ) {
-    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+    val uriHandler = LocalUriHandler.current
     val scrollState = rememberScrollState()
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+    val notice = uiState.showDetail.notice
+
+    // url offset 식별을 위한 코드
+    val urlPattern = "(https?://\\S+\\b)".toRegex().toPattern()
+    val linkMatch = urlPattern.matcher(notice)
+    val urlOffsets = mutableListOf<Pair<Int, Int>>()
+
+    while (linkMatch.find()) {
+        urlOffsets.add(linkMatch.start() to linkMatch.end())
+    }
+
+    val annotatedString = buildAnnotatedString {
+        append(notice)
+        urlOffsets.forEach { (start, end) ->
+            addStyle(
+                SpanStyle(
+                    textDecoration = TextDecoration.Underline,
+                    color = Color(0xFF46A6FF)
+                ),
+                start,
+                end,
+            )
+        }
+    }
 
     Scaffold(
         modifier = modifier,
@@ -36,14 +66,21 @@ fun ShowDetailContentScreen(
             )
         },
     ) { innerPadding ->
-        Text(
+        ClickableText(
             modifier = Modifier
                 .verticalScroll(state = scrollState)
                 .padding(innerPadding)
                 .padding(horizontal = marginHorizontal)
                 .padding(top = 20.dp),
-            text = uiState.showDetail.notice,
+            text = annotatedString,
             style = MaterialTheme.typography.bodyLarge.copy(color = Grey30),
-        )
+        ) { offset ->
+            val urlOffset = urlOffsets.find { (start, end) -> offset in start..<end }
+            if (urlOffset == null) return@ClickableText
+            val (start, end) = urlOffset
+            val url = notice.substring(start, end)
+
+            uriHandler.openUri(url)
+        }
     }
 }

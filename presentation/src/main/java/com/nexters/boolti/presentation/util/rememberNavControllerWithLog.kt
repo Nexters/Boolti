f5cc4c975f29a3f5d101f21b5f0ea7265918a57e
mangbaam <pmb0836@gmail.com>
2024-08-05 03:45:16 +0900

    Boolti-279 fix: FA 로그 데이터 타입 수정 (map -> bundle)

--- presentation/src/main/java/com/nexters/boolti/presentation/util/rememberNavControllerWithLog.kt
+++ presentation/src/main/java/com/nexters/boolti/presentation/util/rememberNavControllerWithLog.kt
@@ -1,13 +1,15 @@
 package com.nexters.boolti.presentation.util
 
+import android.os.Bundle
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.core.os.bundleOf
 import androidx.navigation.NavDestination
 import androidx.navigation.NavHostController
 import androidx.navigation.Navigator
 import androidx.navigation.compose.rememberNavController
+import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.analytics.logEvent
 import com.google.firebase.ktx.Firebase
 import timber.log.Timber
 
@@ -20,22 +22,23 @@ fun rememberNavControllerWithLog(
     LaunchedEffect(navController) {
         navController.currentBackStackEntryFlow
             .collect {
-                val args = it.arguments?.keySet()?.fold(mutableMapOf<String, String>()) { map, key ->
-                    map.apply {
-                        it.arguments?.get(key)?.let { put(key, it.toString()) }
+                val screenName = it.destination.route?.substringBefore('/') ?: ""
+                val args = it.arguments?.keySet()?.fold(Bundle()) { bundle, key ->
+                    if (key == "android-support-nav:controller:deepLinkIntent") return@fold bundle
+                    bundle.apply {
+                        it.arguments?.get(key)?.let { arg -> putString(key, arg.toString()) }
                     }
-                }?.filterKeys { it != "android-support-nav:controller:deepLinkIntent" }
+                }?.let { if (it.isEmpty) null else it }
 
                 Timber.tag("MANGBAAM-(rememberNavControllerWithLog)")
-                    .d("route: ${it.destination.route}, arguments: $args")
+                    .d("screenName: $screenName, arguments: $args")
 
                 Firebase.analytics.logEvent(
-                    "screen",
-                    bundleOf(
-                        "route" to it.destination.route,
-                        "arguments" to args
-                    ),
-                )
+                    FirebaseAnalytics.Event.SCREEN_VIEW,
+                ) {
+                    param(FirebaseAnalytics.Param.SCREEN_NAME, screenName)
+                    args?.let { args -> param("arguments", args) }
+                }
             }
     }
     return navController

c5471e897d07cf2a5a3077a00915ee93beeb17ae
algosketch <algosketch@gmail.com>
2024-09-04 15:28:52 +0900

    feat : 선물하기 웹뷰 방침 적용

---
+++ presentation/src/main/java/com/nexters/boolti/presentation/component/PolicyBottomSheet.kt
@@ -0,0 +1,54 @@
+package com.nexters.boolti.presentation.component
+
+import android.annotation.SuppressLint
+import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import com.nexters.boolti.presentation.theme.Grey70
+
+@SuppressLint("SetJavaScriptEnabled")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PolicyBottomSheet(
+    onDismissRequest: () -> Unit,
+    url: String,
+) {
+    ModalBottomSheet(
+        onDismissRequest = onDismissRequest,
+        dragHandle = {
+            Box(
+                modifier = Modifier
+                    .padding(top = 12.dp)
+                    .size(45.dp, 4.dp)
+                    .clip(CircleShape)
+                    .background(Grey70),
+            )
+        },
+    ) {
+        AndroidView(
+            factory = { context ->
+                WebView(context).apply {
+                    layoutParams = ViewGroup.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.MATCH_PARENT
+                    )
+                    webViewClient = WebViewClient()
+                    settings.javaScriptEnabled = true
+                    settings.domStorageEnabled = true
+                    loadUrl(url)
+                }
+            })
+    }
+}

fe40348030bb3875d2b3457dc4003a0ceee76bdb
algosketch <algosketch@gmail.com>
2024-10-17 01:28:46 +0900

    fix : 버튼 위치 스크롤 맨 아래로 수정

--- presentation/src/main/java/com/nexters/boolti/presentation/screen/giftcomplete/GiftCompleteScreen.kt
+++ presentation/src/main/java/com/nexters/boolti/presentation/screen/giftcomplete/GiftCompleteScreen.kt
@@ -3,12 +3,10 @@ package com.nexters.boolti.presentation.screen.giftcomplete
 import android.content.Context
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -22,6 +20,7 @@ import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
@@ -29,13 +28,13 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringArrayResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -50,9 +49,11 @@ import com.nexters.boolti.domain.model.ReservationDetail
 import com.nexters.boolti.presentation.BuildConfig
 import com.nexters.boolti.presentation.R
 import com.nexters.boolti.presentation.component.SecondaryButton
+import com.nexters.boolti.presentation.component.dummyReservationDetail
 import com.nexters.boolti.presentation.extension.cardCodeToCompanyName
 import com.nexters.boolti.presentation.screen.payment.PaymentToolbar
 import com.nexters.boolti.presentation.screen.payment.TicketSummarySection
+import com.nexters.boolti.presentation.theme.BooltiTheme
 import com.nexters.boolti.presentation.theme.Grey15
 import com.nexters.boolti.presentation.theme.Grey30
 import com.nexters.boolti.presentation.theme.Grey40
@@ -70,8 +71,24 @@ fun GiftCompleteScreen(
     onClickClose: () -> Unit,
     navigateToReservation: (reservation: ReservationDetail) -> Unit,
     viewModel: GiftCompleteViewModel = hiltViewModel(),
-) {
+)  {
     val reservation by viewModel.reservation.collectAsStateWithLifecycle()
+
+    GiftCompleteScreen(
+        onClickHome = onClickHome,
+        onClickClose = onClickClose,
+        navigateToReservation = navigateToReservation,
+        reservation = reservation,
+    )
+}
+
+@Composable
+fun GiftCompleteScreen(
+    onClickHome: () -> Unit,
+    onClickClose: () -> Unit,
+    navigateToReservation: (reservation: ReservationDetail) -> Unit,
+    reservation: ReservationDetail?,
+) {
     val context = LocalContext.current
 
     BackHandler(onBack = onClickClose)
@@ -79,7 +96,7 @@ fun GiftCompleteScreen(
     Scaffold(
         topBar = {
             PaymentToolbar(onClickHome = onClickHome, onClickClose = onClickClose)
-        }
+        },
     ) { innerPadding ->
         Box {
             Column(
@@ -90,7 +107,10 @@ fun GiftCompleteScreen(
             ) {
                 val month = reservation?.salesEndDateTime?.month?.value ?: 0
                 val day = reservation?.salesEndDateTime?.dayOfMonth ?: 0
-                val senderText = stringResource(id = R.string.gift_sender_description, reservation?.depositorName ?: "")
+                val senderText = stringResource(
+                    id = R.string.gift_sender_description,
+                    reservation?.depositorName ?: ""
+                )
                 val dateText = stringResource(id = R.string.gift_expiration_date, month, day)
                 val buttonText = stringResource(id = R.string.gift_check)
 
@@ -156,26 +176,11 @@ fun GiftCompleteScreen(
                     ShowInformation(
                         reservation = reservation
                     )
-                }
-                Spacer(modifier = Modifier.height(88.dp))
-            }
-            reservation?.let { reservation ->
-                Column(
-                    modifier = Modifier.align(Alignment.BottomCenter)
-                ) {
-                    Box(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .height(16.dp)
-                            .background(
-                                brush = Brush.verticalGradient(listOf(Color.Transparent, Grey95))
-                            )
-                    )
                     SecondaryButton(
                         modifier = Modifier
                             .fillMaxWidth()
-                            .background(Grey95)
-                            .padding(horizontal = marginHorizontal, vertical = 8.dp),
+                            .padding(top = 16.dp)
+                            .padding(vertical = 8.dp),
                         label = stringResource(R.string.show_reservation),
                     ) {
                         navigateToReservation(reservation)
@@ -372,5 +377,21 @@ private fun ShowInformation(
             showName = reservation.showName,
             showDate = reservation.showDate,
         )
+    }
+}
+
+@Composable
+@Preview
+fun GiftCompleteScreenPreview(
+) {
+    BooltiTheme {
+        Surface {
+            GiftCompleteScreen(
+                onClickClose = {},
+                onClickHome = {},
+                navigateToReservation = {},
+                reservation = dummyReservationDetail
+            )
+        }
     }
 }

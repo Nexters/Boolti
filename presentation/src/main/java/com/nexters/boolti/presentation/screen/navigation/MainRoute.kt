b3174f6be7f4fe02b3b537dfe2c476d1df317c2d
algosketch <algosketch@gmail.com>
2024-10-21 00:05:27 +0900

    add : main route 정의

---
+++ presentation/src/main/java/com/nexters/boolti/presentation/screen/navigation/MainRoute.kt
@@ -0,0 +1,92 @@
+package com.nexters.boolti.presentation.screen.navigation
+
+import kotlinx.serialization.Serializable
+
+sealed interface MainRoute {
+    @Serializable
+    data object Home : MainRoute
+
+    @Serializable
+    data class ShowDetail(
+        val showId: String,
+    ) : MainRoute
+
+    @Serializable
+    data class Ticketing(
+        val showId: String,
+        val salesTicketId: String,
+        val ticketCount: Int,
+        val isInviteTicket: Boolean,
+    ) : MainRoute
+
+    @Serializable
+    data class Gift(
+        val showId: String,
+        val salesTicketId: String,
+        val ticketCount: Int,
+    ) : MainRoute
+
+    @Serializable
+    data class PaymentComplete(
+        val reservationId: String,
+        val showId: String,
+    ) : MainRoute
+
+    @Serializable
+    data class GiftComplete(
+        val giftId: String,
+    ) : MainRoute
+
+    @Serializable
+    data class TicketDetail(
+        val ticketId: String,
+    )
+
+    @Serializable
+    data object Qr : MainRoute
+
+    @Serializable
+    data object Reservations : MainRoute
+
+    @Serializable
+    data class ReservationDetail(
+        val reservationId: String,
+        val isGift: Boolean,
+    ) : MainRoute
+
+    @Serializable
+    data class Refund(
+        val reservationId: String,
+        val isGift: Boolean
+    ) : MainRoute
+
+    @Serializable
+    data object HostedShows : MainRoute
+
+    @Serializable
+    data object SignOut : MainRoute
+
+    @Serializable
+    data object Login : MainRoute
+
+    @Serializable
+    data object Business : MainRoute
+
+    @Serializable
+    data object AccountSetting : MainRoute
+
+    @Serializable
+    data class Profile(
+        val userCode: String?
+    ) : MainRoute
+
+    @Serializable
+    data object ProfileEdit : MainRoute
+
+    @Serializable
+    data class ProfileLinkEdit(
+        val linkId: String?,
+        val linkTitle: String?,
+        val url: String?
+    ) : MainRoute
+}

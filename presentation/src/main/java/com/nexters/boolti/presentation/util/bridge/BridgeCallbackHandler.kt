a4d86574eebd39ca86ef7b5f85ba78d9debafb8f
mangbaam <pmb0836@gmail.com>
2024-12-15 22:56:17 +0900

    feature/Boolti-347 docs: 브릿지 관련 코드 주석 추가

--- presentation/src/main/java/com/nexters/boolti/presentation/util/bridge/BridgeCallbackHandler.kt
+++ presentation/src/main/java/com/nexters/boolti/presentation/util/bridge/BridgeCallbackHandler.kt
@@ -2,6 +2,11 @@ package com.nexters.boolti.presentation.util.bridge
 
 import kotlinx.serialization.Serializable
 
+/**
+ * 브릿지를 통해 받은 데이터로부터 처리해야 하는 액션을 정의하는 핸들러
+ *
+ * 새로운 액션이 정의되는 경우 [BridgeManager.handleIncomingData] 도 함께 수정되어야 한다.
+ */
 interface BridgeCallbackHandler {
     fun fetchToken(): TokenDto
 }
--- presentation/src/main/java/com/nexters/boolti/presentation/util/bridge/BridgeManager.kt
+++ presentation/src/main/java/com/nexters/boolti/presentation/util/bridge/BridgeManager.kt
@@ -21,8 +21,16 @@ class BridgeManager(
     private val _dataToSendToWeb = Channel<String>()
     val dataToSendWeb = _dataToSendToWeb.receiveAsFlow()
 
+    /**
+     * 웹뷰에서 호출하는 메서드
+     *
+     * 웹 브릿지를 통해 받은 데이터의 타입에 따라 [BridgeCallbackHandler]의 메서드를 호출한다.
+     * 이후 함수 내부에서 [BridgeCallbackHandler]의 메서드로부터 받은 결과를 웹으로 콜백한다.
+     *
+     * @param data
+     */
     fun handleIncomingData(data: BridgeDto) {
-        originDataMap[data.id] = data
+        originDataMap[data.id] = data // 디버깅을 위한 캐싱
 
         when (data.command) {
             CommandType.REQUEST_TOKEN -> {
@@ -34,12 +42,27 @@ class BridgeManager(
         }
     }
 
+    /**
+     * WebView 에서 웹에 evaluation 할 자바스크립트 코드 전달
+     *
+     * @param data
+     */
     fun sendDataToWeb(data: String) {
         scope.launch {
             _dataToSendToWeb.send("javascript:$webCallbackName($data)")
         }
     }
 
+    /**
+     * 웹에서 브릿지를 통해 데이터를 받은 경우, 웹으로 콜백 해줘야 한다. (콜백 해주지 않으면 웹의 비동기 코드가 종료되지 않음)
+     *
+     * 예를 들어 화면 이동의 경우 웹으로 전달할 데이터가 없으니 [BridgeDto.timestamp]를 제외한 나머지 정보는 에코 방식으로 그대로 콜백한다.
+     *
+     * 토큰 요청과 같이 웹으로 전달할 데이터가 있는 경우 [BridgeDto.data]에 웹과 맞춘 형태로 데이터를 포함시키고, 나머지 [BridgeDto.id]와 [BridgeDto.command]는 그대로 콜백한다.
+     *
+     * @param originData 웹에서 브릿지를 통해 받은 원본 데이터
+     * @param responseData [BridgeDto.data]에 포함될 데이터. `null` 이라면 원본 [BridgeDto.data] 를 사용한다.
+     */
     private fun callbackToWeb(
         originData: BridgeDto,
         responseData: JsonElement?
@@ -48,6 +71,8 @@ class BridgeManager(
             data = responseData
         )
         val json = json.encodeToString(responseDto)
+            .replace("\"{\"", "{")
+            .replace("\"}\"", "}")
         sendDataToWeb(json)
     }
 
@@ -67,8 +92,8 @@ class BridgeManager(
 private data class BridgeCallbackDto(
     val id: String,
     val command: CommandType,
-    val data: String,
     val timestamp: Long = System.currentTimeMillis(),
+    val data: String?,
 )
 
 private fun BridgeDto.toBridgeCallbackDto(data: JsonElement? = null): BridgeCallbackDto {
@@ -76,7 +101,7 @@ private fun BridgeDto.toBridgeCallbackDto(data: JsonElement? = null): BridgeCall
         isLenient = true
         ignoreUnknownKeys = true
     }
-    val dataJsonElement = data ?: this.data ?: json.encodeToJsonElement(Unit)
+    val dataJsonElement = data ?: this.data
     return BridgeCallbackDto(
         id = id,
         command = command,

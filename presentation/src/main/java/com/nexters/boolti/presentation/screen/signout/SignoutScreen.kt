6cbbc04db92b2a4703c303f35dcd6922ec253bdf
mangbaam <pmb0836@gmail.com>
2024-12-22 02:00:58 +0900

    Boolti-343 style: 상태바 패딩 대응
    - 회원 탈퇴

--- presentation/src/main/java/com/nexters/boolti/presentation/screen/signout/SignoutScreen.kt
+++ presentation/src/main/java/com/nexters/boolti/presentation/screen/signout/SignoutScreen.kt
@@ -3,6 +3,7 @@ package com.nexters.boolti.presentation.screen.signout
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -42,6 +43,7 @@ fun SignoutScreen(
     }
 
     Scaffold(
+        modifier = Modifier.systemBarsPadding(),
         topBar = { BtBackAppBar(title = stringResource(R.string.signout), onClickBack = navigateBack) },
         bottomBar = {
             MainButton(

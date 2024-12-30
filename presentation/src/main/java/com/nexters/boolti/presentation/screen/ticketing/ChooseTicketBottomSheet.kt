b76355d3d048435e1e5e991387b24088017768a1
mangbaam <pmb0836@gmail.com>
2024-12-31 01:00:58 +0900

    Boolti-343 style: 티켓 선택 바텀시트 불필요한 하단 패딩 제거

--- presentation/src/main/java/com/nexters/boolti/presentation/screen/ticketing/ChooseTicketBottomSheet.kt
+++ presentation/src/main/java/com/nexters/boolti/presentation/screen/ticketing/ChooseTicketBottomSheet.kt
@@ -6,12 +6,9 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
-import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -104,11 +101,7 @@ fun ChooseTicketBottomSheet(
     ) {
         Column(
             modifier = Modifier
-                .padding(
-                    bottom = 20.dp + WindowInsets.navigationBars
-                        .asPaddingValues()
-                        .calculateBottomPadding()
-                )
+                .padding(bottom = 20.dp)
                 .heightIn(max = 564.dp)
         ) {
             uiState.selected?.let {

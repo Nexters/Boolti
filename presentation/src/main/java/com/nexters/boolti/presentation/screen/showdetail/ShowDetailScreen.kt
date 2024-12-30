82d6e8b60e18ce30574d28ec2febfd048d266c5b
mangbaam <pmb0836@gmail.com>
2024-12-31 00:45:04 +0900

    Boolti-343 fix: 머지 충돌 해결 후 공연 상세 UI 대응

--- presentation/src/main/java/com/nexters/boolti/presentation/screen/showdetail/ShowDetailScreen.kt
+++ presentation/src/main/java/com/nexters/boolti/presentation/screen/showdetail/ShowDetailScreen.kt
@@ -21,6 +21,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
@@ -156,7 +157,7 @@ fun ShowDetailScreen(
 
     CompositionLocalProvider(LocalOverscrollConfiguration provides null) {
         Scaffold(
-            modifier = modifier,
+            modifier = modifier.navigationBarsPadding(),
             topBar = {
                 ShowDetailAppBar(
                     showDetail = uiState.showDetail,

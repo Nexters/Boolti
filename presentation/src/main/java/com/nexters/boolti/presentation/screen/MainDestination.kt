a5c5217e7d135e5c716f907d31155500bd202d25
mangbaam <pmb0836@gmail.com>
2024-12-31 21:04:27 +0900

    refactor/253 todo 추가

--- presentation/src/main/java/com/nexters/boolti/presentation/screen/MainDestination.kt
+++ presentation/src/main/java/com/nexters/boolti/presentation/screen/MainDestination.kt
@@ -1,5 +1,6 @@
 package com.nexters.boolti.presentation.screen
 
 sealed class MainDestination(val route: String) {
+    // TODO 브릿지 작업 머지 후 작업
     data object ShowRegistration : MainDestination(route = "webView")
 }

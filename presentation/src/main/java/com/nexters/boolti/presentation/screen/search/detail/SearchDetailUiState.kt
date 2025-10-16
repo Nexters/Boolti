171bfe65fee293bf2cddf3249d801dc4eb22ecf4
mangbaam <pmb0836@gmail.com>
2025-10-17 02:53:46 +0900

    Boolti-405 검색 Repository 연결

--- presentation/src/main/java/com/nexters/boolti/presentation/screen/search/detail/SearchDetailUiState.kt
+++ presentation/src/main/java/com/nexters/boolti/presentation/screen/search/detail/SearchDetailUiState.kt
@@ -3,7 +3,6 @@ package com.nexters.boolti.presentation.screen.search.detail
 import androidx.compose.runtime.Stable
 import com.nexters.boolti.domain.model.Show
 import com.nexters.boolti.domain.model.User
-import java.time.LocalDateTime
 
 @Stable
 data class SearchDetailUiState(
@@ -19,88 +18,8 @@ data class SearchDetailUiState(
             loading = false,
             keyword = "",
             searchedKeyword = "",
-            shows = listOf(
-                Show(
-                    id = "1",
-                    name = "2024 TOGETHER LUCKY CLUB",
-                    date = LocalDateTime.now(),
-                    salesEndDate = null,
-                    salesStartDate = null,
-                    thumbnailImage = "",
-                ),
-                Show(
-                    id = "2",
-                    name = "TUNE Project No.3 TUNE’s Halloween Party",
-                    date = LocalDateTime.now(),
-                    salesEndDate = null,
-                    salesStartDate = null,
-                    thumbnailImage = "",
-                ),
-                Show(
-                    id = "3",
-                    name = "쇼팽과 라벨",
-                    date = LocalDateTime.now(),
-                    salesEndDate = null,
-                    salesStartDate = null,
-                    thumbnailImage = "",
-                ),
-                Show(
-                    id = "4",
-                    name = "2024 TOGETHER LUCKY CLUB",
-                    date = LocalDateTime.now(),
-                    salesEndDate = null,
-                    salesStartDate = null,
-                    thumbnailImage = "",
-                ),
-                Show(
-                    id = "5",
-                    name = "TUNE Project No.3 TUNE’s Halloween Party",
-                    date = LocalDateTime.now(),
-                    salesEndDate = null,
-                    salesStartDate = null,
-                    thumbnailImage = "",
-                ),
-                Show(
-                    id = "6",
-                    name = "쇼팽과 라벨",
-                    date = LocalDateTime.now(),
-                    salesEndDate = null,
-                    salesStartDate = null,
-                    thumbnailImage = "",
-                ),
-            ),
-            profiles = listOf(
-                User.Others(
-                    nickname = "아티스트 닉네임은 최대 12자",
-                    photo = null,
-                    userCode = "1",
-                ),
-                User.Others(
-                    nickname = "김람지",
-                    photo = null,
-                    userCode = "2",
-                ),
-                User.Others(
-                    nickname = "스튜디오 불티",
-                    photo = null,
-                    userCode = "3",
-                ),
-                User.Others(
-                    nickname = "아티스트 닉네임은 최대 12자",
-                    photo = null,
-                    userCode = "4",
-                ),
-                User.Others(
-                    nickname = "김람지",
-                    photo = null,
-                    userCode = "5",
-                ),
-                User.Others(
-                    nickname = "스튜디오 불티",
-                    photo = null,
-                    userCode = "6",
-                ),
-            ),
+            shows = emptyList(),
+            profiles = emptyList(),
             tabIndex = 0,
         )
     }
--- presentation/src/main/java/com/nexters/boolti/presentation/screen/search/detail/SearchDetailViewModel.kt
+++ presentation/src/main/java/com/nexters/boolti/presentation/screen/search/detail/SearchDetailViewModel.kt
@@ -4,23 +4,24 @@ import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.viewModelScope
 import androidx.navigation.toRoute
 import com.nexters.boolti.domain.repository.SearchHistoryRepository
+import com.nexters.boolti.domain.repository.SearchRepository
 import com.nexters.boolti.presentation.base.BaseViewModel
 import com.nexters.boolti.presentation.screen.navigation.SearchRoute
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
-import kotlinx.coroutines.delay
+import kotlinx.coroutines.async
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
-import kotlin.time.Duration.Companion.seconds
 
 @HiltViewModel
 class SearchDetailViewModel @Inject constructor(
     savedStateHandle: SavedStateHandle,
     private val searchHistoryRepository: SearchHistoryRepository,
+    private val searchRepository: SearchRepository,
 ) : BaseViewModel() {
     private val route = savedStateHandle.toRoute<SearchRoute.SearchDetail>()
 
@@ -50,15 +51,29 @@ class SearchDetailViewModel @Inject constructor(
         searchJob = viewModelScope.launch {
             _uiState.update { it.copy(loading = true) }
 
-            delay(2.seconds)
             launch(Dispatchers.IO) {
                 searchHistoryRepository.saveSearchHistory(keyword)
             }
 
-            // TODO 검색
+            // 공연 검색과 프로필 검색을 동시에 실행
+            val showsDeferred = async(Dispatchers.IO) {
+                searchRepository.searchShows(keyword)
+            }
+
+            val profilesDeferred = async(Dispatchers.IO) {
+                searchRepository.searchProfiles(keyword)
+            }
+
+            // 두 결과를 모두 기다림
+            val showsResult = showsDeferred.await()
+            val profilesResult = profilesDeferred.await()
+
+            // UiState 업데이트
             _uiState.update {
                 it.copy(
                     searchedKeyword = keyword,
+                    shows = showsResult.getOrElse { emptyList() },
+                    profiles = profilesResult.getOrElse { emptyList() },
                     loading = false,
                 )
             }

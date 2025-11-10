8b93d5c7a1355b761d60a34e0a8e85076eeb3477
HamBP <sjy_9484@naver.com>
2025-11-11 00:07:16 +0900

    fix: deprecation 된 api를 제거했어요.

--- presentation/src/main/java/com/nexters/boolti/presentation/component/BoBottomSheet.kt
+++ presentation/src/main/java/com/nexters/boolti/presentation/component/BoBottomSheet.kt
@@ -1,24 +1,24 @@
 package com.nexters.boolti.presentation.component
 
-import android.app.Activity
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.unit.dp
 import com.nexters.boolti.presentation.theme.Grey70
 
@@ -28,23 +28,10 @@ fun BtBottomSheet(
     onDismissRequest: () -> Unit,
     content: @Composable ColumnScope.() -> Unit,
 ) {
-    val view = LocalView.current
-    val navigationBarColor = MaterialTheme.colorScheme.surfaceTint
-
-    DisposableEffect(Unit) {
-        val window = (view.context as Activity).window
-        val originalNavBarColor = window.navigationBarColor
-
-        window.navigationBarColor = navigationBarColor.toArgb()
-
-        onDispose {
-            window.navigationBarColor = originalNavBarColor
-        }
-    }
-
     ModalBottomSheet(
         containerColor = MaterialTheme.colorScheme.surfaceTint,
         onDismissRequest = onDismissRequest,
+        windowInsets = WindowInsets(0, 0, 0, 0),
         dragHandle = {
             Box(
                 modifier = Modifier
@@ -57,6 +44,6 @@ fun BtBottomSheet(
         shape = RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp),
     ) {
         content()
-        Spacer(Modifier.height(28.dp))
+        Spacer(Modifier.windowInsetsPadding(WindowInsets.navigationBars))
     }
 }

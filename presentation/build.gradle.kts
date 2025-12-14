713edc5cb88288b0416806c39e18f6b4a46af8b2
mangbaam <pmb0836@gmail.com>
2025-12-14 18:09:18 +0900

    Boolti-422 Navigation3 의존성  추가

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -91,6 +91,7 @@ dependencies {
 
     implementation(libs.hilt.android)
     implementation(libs.androidx.hilt.navigation.compose)
+    implementation(libs.androidx.lifecycle.viewmodel.navigation3)
     implementation(libs.kotlinx.serialization.json)
     implementation(libs.androidx.material3.android)
     implementation(libs.zoomable)

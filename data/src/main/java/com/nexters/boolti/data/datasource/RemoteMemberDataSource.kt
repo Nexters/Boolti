907269c016e941965333a56c9c484ae324c39bc1
HamBP <sjy_9484@naver.com>
2025-12-15 21:26:21 +0900

    fix: 공연을 받아올 때 역직렬화 에러가 발생하던 문제를 수정했어요.

--- data/src/main/java/com/nexters/boolti/data/datasource/RemoteMemberDataSource.kt
+++ data/src/main/java/com/nexters/boolti/data/datasource/RemoteMemberDataSource.kt
@@ -3,6 +3,7 @@ package com.nexters.boolti.data.datasource
 import com.nexters.boolti.data.network.api.MemberService
 import com.nexters.boolti.data.network.response.HostedShowDto
 import com.nexters.boolti.data.network.response.MemberResponse
+import com.nexters.boolti.data.network.response.PreviousShowsResponse
 import com.nexters.boolti.domain.model.UserCode
 import com.nexters.boolti.domain.request.EditProfileRequest
 import javax.inject.Inject
@@ -12,6 +13,6 @@ internal class RemoteMemberDataSource @Inject constructor(
 ) {
     suspend fun getMember(userCode: UserCode): MemberResponse = memberService.getMember(userCode)
     suspend fun getLinks(userCode: UserCode): List<EditProfileRequest.LinkDto> = memberService.getLinks(userCode)
-    suspend fun getPerformedShows(userCode: UserCode): List<HostedShowDto> = memberService.getPerformedShows(userCode)
+    suspend fun getPerformedShows(userCode: UserCode): List<PreviousShowsResponse> = memberService.getPerformedShows(userCode)
     suspend fun getVideoLinks(userCode: UserCode): List<String> = memberService.getVideoLinks(userCode)
 }
--- data/src/main/java/com/nexters/boolti/data/network/response/PreviousShowsResponse.kt
+++ data/src/main/java/com/nexters/boolti/data/network/response/PreviousShowsResponse.kt
@@ -4,20 +4,19 @@ import com.nexters.boolti.data.util.toLocalDate
 import com.nexters.boolti.data.util.toLocalDateTime
 import com.nexters.boolti.domain.model.Show
 import kotlinx.serialization.SerialName
-import kotlinx.serialization.Serializable
 
-@Serializable
-internal data class HostedShowDto(
-    @SerialName("showId") val showId: String,
-    @SerialName("showName") val showName: String,
+data class PreviousShowsResponse(
+    @SerialName("id") val id: String,
+    @SerialName("name") val name: String,
     @SerialName("date") val date: String,
     @SerialName("salesStartTime") val salesStartDate: String?,
     @SerialName("salesEndTime") val salesEndDate: String?,
     @SerialName("showImg") val thumbnailImage: String,
+    @SerialName("isNonTicketing") val isNonTicketing: Boolean,
 ) {
     fun toDomain(): Show = Show(
-        id = showId,
-        name = showName,
+        id = id,
+        name = name,
         date = date.toLocalDateTime(),
         salesStartDate = salesStartDate?.toLocalDate(),
         salesEndDate = salesEndDate?.toLocalDate(),

a5ea84f0a1d25e1cb42341d96927ed77e6dbb8f9
박명범 <mangbaam@bagmyeongbeom-ui-Macmini.local>
2026-03-22 22:45:25 +0900

    Add underline style to profile user code

--- presentation/src/main/java/com/nexters/boolti/presentation/screen/profile/ProfileScreen.kt
+++ presentation/src/main/java/com/nexters/boolti/presentation/screen/profile/ProfileScreen.kt
@@ -59,6 +59,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp

32e06913aa702284282bdb84ce16b280a20d9cb3
algosketch <algosketch@gmail.com>
2026-04-02 02:58:45 +0900

    refactor: 정규표현식을 컴포저블 바깥으로 뺐어요.

--- presentation/src/main/java/com/nexters/boolti/presentation/screen/Main.kt
+++ presentation/src/main/java/com/nexters/boolti/presentation/screen/Main.kt
@@ -85,6 +85,7 @@ val LocalNavController = compositionLocalOf<NavHostController> {
 }
 
 val LocalUser = compositionLocalOf<User?> { null }
+private val giftDeepLinkRegex = "^boolti://gift/([\\w-]+)$".toRegex()
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
@@ -98,7 +99,6 @@ fun Main(
     val rootNavController = rememberNavControllerWithLog()
     val activity = LocalActivity.current as ComponentActivity
     val giftDeepLinkViewModel: GiftDeepLinkViewModel = hiltViewModel(activity)
-    val giftDeepLinkRegex = remember { "^boolti://gift/([\\w-]+)$".toRegex() }
 
     LaunchedEffect(Unit) {
         val deepLink = activity.intent?.data?.toString() ?: return@LaunchedEffect

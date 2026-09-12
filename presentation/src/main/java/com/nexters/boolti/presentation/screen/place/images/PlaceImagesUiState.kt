74f2ba73f74ead31e92adfc3ba2ff08e6b22aa92
algosketch <algosketch@gmail.com>
2026-09-12 17:51:24 +0900

    feat: Stable 어노테이션 추가

--- presentation/src/main/java/com/nexters/boolti/presentation/screen/place/images/PlaceImagesUiState.kt
+++ presentation/src/main/java/com/nexters/boolti/presentation/screen/place/images/PlaceImagesUiState.kt
@@ -1,7 +1,9 @@
 package com.nexters.boolti.presentation.screen.place.images
 
+import androidx.compose.runtime.Stable
 import com.nexters.boolti.domain.model.PlaceImage
 
+@Stable
 data class PlaceImagesUiState(
     val images: List<PlaceImage> = emptyList(),
     val isLoading: Boolean = true,
